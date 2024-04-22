9073d32460e701386d17ac1ef6dbbe0254336c8f
ltzmk06 <152815564+ltzmk06@users.noreply.github.com>
2024-04-22 14:52:55 +0530

    some minor animations added

---
+++ script.js
@@ -0,0 +1,27 @@
+Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
+
+  Shery.imageMasker("#me-img" , {
+    mouseFollower: true,
+    text: "Manoj",
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+  Shery.makeMagnet(".nav-item", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+  Shery.textAnimate("#main-title, #subtitle",{
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
+
